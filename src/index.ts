574e7a98cda6e857bf860c1bb0a2252c031ae8df
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2024-05-13 01:59:05 -0300

    🔥 Remove template code

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,6 @@
 // and on native platforms to ExpoMediastore.ts
 import ExpoMediastoreModule from './ExpoMediastoreModule';
 
-// Get the native constant value.
-export const PI = ExpoMediastoreModule.PI;
-
-export function hello(): string {
-  return ExpoMediastoreModule.hello();
-}
-
 interface MusicMedia {
   id: number;
   name: string;
